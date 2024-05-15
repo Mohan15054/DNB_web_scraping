454dc0f393efa37a6af4dd46eb0cd78ac5c30971
Mohan M <114956287+Mohan-rep@users.noreply.github.com>
2024-05-15 23:48:38 +0530

    Update storing csv file

--- Main.py
+++ Main.py
@@ -1,32 +1,47 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import post_process
+import os
 
-# Initialize the WebDriver
+
+# Max Page numbe:
+
+page=10
+result_file_name = 'main_data_result1.csv'
+
+
 driver = webdriver.Chrome()
 
 # Open a new tab
 driver.execute_script("window.open('https://www.dnb.com/business-directory/company-information.manufacturing.in.html?page=3');")
 
-time.sleep(30)
+time.sleep(20)
 # Switch to the new tab
 driver.switch_to.window(driver.window_handles[1])
 
 # Navigate to a URL
 
-for i in range(1,5):
+for i in range(1,page):
     print(i)
+    # driver.switch_to.window(driver.window_handles[2])
+    try:
+        driver.get(f"https://www.dnb.com/business-directory/company-information.manufacturing.in.html?page={i}")
+        time.sleep(15)
+        page_source = driver.page_source
 
-    driver.get(f"https://www.dnb.com/business-directory/company-information.manufacturing.in.html?page={i}")
+        # Save the page source to an HTML file
+        with open(f"page_source_tes{i}.html", "w", encoding="utf-8") as file:
+            file.write(page_source)
 
-    page_source = driver.page_source
+        post_process.process_html(result_file_name,f'page_source_tes{i}.html')
+        print(f"Page - {i} feched ")
+        os.remove(f'page_source_tes{i}.html')
+    except Exception as e:
+        print(e)
 
-    # Save the page source to an HTML file
-    with open(f"page_source_tes{i}.html", "w", encoding="utf-8") as file:
-        file.write(page_source)
-
-    # Wait for a few seconds (optional)
-time.sleep(300)
+print(f"Finished processing {page} completed")
+time.sleep(30)
 
 # Close the browser
 driver.quit()
--- post_process.py
+++ post_process.py
@@ -1,25 +1,62 @@
 from bs4 import BeautifulSoup
 import csv
 
-# Read HTML file
-with open('page_source.html', 'r',encoding='utf-8') as html_file:
-    html_content = html_file.read()
+def remove_empty_space(string):
+        return ' '.join(string.split())
 
-# Parse HTML
-soup = BeautifulSoup(html_content, 'html.parser')
 
-# Find the table
-table = soup.find('table')
-print(table)
-# Open CSV file for writing
-with open('table_data.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
+def write_csv(filename,data): 
+    try:
+        # filename='main_data_result.csv'
+        with open(filename, 'a', newline='') as csvfile:
+                fieldnames = list(data.keys())
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                # If the file is empty, write the header
+                if csvfile.tell() == 0:
+                    writer.writeheader()
+                # Write the data
+                writer.writerow({key: remove_empty_space(value) for key, value in data.items()})
+    except Exception as e:
+         print("Error writing data  to file :",e)
 
-    # Loop through rows
-    for row in table.find_all('tr'):
-        # Extract data from each cell in the row
-        data = [cell.get_text(strip=True) for cell in row.find_all(['td', 'th'])]
-        # Write data to CSV file
-        writer.writerow(data)
+def process_html(file_name,html_file):
+    try:
+        with open(html_file, 'r', encoding='utf-8') as file:
+            html_content = file.read()
 
-print("CSV file generated successfully.")
+        # Parse the HTML content
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        # Find the table
+        divs = soup.find_all('div', class_='col-md-12 data')
+
+        print(len(divs))
+    except Exception as e:
+         print(e)
+
+
+    if divs:
+        # print(divs[0])
+        for div in divs:
+            # print(div)
+            column_names = {
+                "Company": None,
+                "Country": None,
+                "Sales Revenue": None
+            }
+            if div:
+                company = div.find('div', class_='col-md-6')
+                if company:
+                    column_names["Company"] = remove_empty_space(company.text.strip())
+
+                country = div.find('div', class_='col-md-4')
+                if country:
+                    column_names["Country"] = remove_empty_space(country.text.replace("Country:", ""))
+
+                sales_revenue = div.find('div', class_='col-md-2 last')
+                if sales_revenue:
+                    column_names["Sales Revenue"] = remove_empty_space(sales_revenue.text.replace("Sales Revenue ($M):", ""))
+            else:
+                print("No div found with the specified name.")
+            # print(column_names)
+            write_csv(file_name,column_names)
